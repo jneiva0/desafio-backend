ceb6b0a9e3433293f0af84e5e4b54631fc845a1e
Joao Neiva <jneiva42@gmail.com>
2022-06-19 02:04:17 -0300

    adicionar module service para restaurantes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from 'src/auth/auth.module'
 import { AuthService } from 'src/auth/auth.service'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UsersService } from 'src/users/users.service'
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RestaurantModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, UsersService, AuthService],
 })
