d65d4e7695b5db2628bd405c70f188c374e0fa4c
Joao Neiva <jneiva42@gmail.com>
2022-06-19 06:09:26 -0300

    configurar Pipe para transformar strings em numeros

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { PrismaService } from 'nestjs-prisma'
 import { AppModule } from './app.module'
@@ -10,6 +11,17 @@ async function bootstrap() {
   const prismaService: PrismaService = app.get(PrismaService)
   prismaService.enableShutdownHooks(app)
   app.enableCors({ origin: ['http://localhost:3000'] })
+
+  // A linha abaixo evita que ocorra erros de validação, exemplo: um parametro id que é um numero mas foi passado como uma string na request
+  // Para casos simples apenas setar "transform: true" resolve. Em um App real seria feita uma configuracao usando DTOs e configurando regras de validacao na API
+  // O conceito de Pipes do NestJS é bem poderoso.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+      transform: true,
+    }),
+  )
+
   await app.listen(4000)
 }
 bootstrap()
